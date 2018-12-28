a7be43515c133650adf5317c67631a682905177a
steamme <nikita258963@gmail.com>
2018-12-28 16:06:26 +0200

    1111

--- bot.php
+++ bot.php
@@ -8,13 +8,12 @@ $result = $telegram -> getWebhookUpdates(); //Передаем в перемен
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-$keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
+
 
 if($text){
     if ($text == "/start") {
         $reply = "Добро пожаловать в бота!";
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
     }elseif ($text == "/open") {
         $reply = "Открываю";
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
