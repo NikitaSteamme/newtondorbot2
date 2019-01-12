41eadd8b85fb3c7b2fd75e2d358522fa1e3afd7f
steamme <nikita258963@gmail.com>
2019-01-12 15:42:08 +0200

    1111

--- bot.php
+++ bot.php
@@ -15,7 +15,30 @@ if($text){
         $reply = "Добро пожаловать в бота!";
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
     }elseif ($text == "/open") {
-        $reply = "Открываю";
+        $url = 'http://31.202.46.87:8080/protect/status.xml';
+
+        $result = file_get_contents($url, false, stream_context_create(array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'login' => 'admin',
+                'password' => 'vkmodule'
+            )
+        )));
+        $url = 'http://31.202.46.87:8080/protect/leds.cgi';
+        $params = array(
+            'led' => '0', // в http://localhost/post.php это будет $_POST['param1'] == '123'
+            'timeout' => '0', // в http://localhost/post.php это будет $_POST['param2'] == 'abc'
+        );
+
+        $result .= file_get_contents($url, false, stream_context_create(array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => http_build_query($params)
+            )
+        )));
+        $reply = $result;
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
     }elseif ($text == "/link") {
         $reply = "тут будет линка";
