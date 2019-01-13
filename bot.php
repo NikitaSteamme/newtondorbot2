5e82156813c40022368a03a78d70fa24cc425649
steamme <nikita258963@gmail.com>
2019-01-13 12:25:07 +0200

    1111

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 <?php
     include('vendor/autoload.php'); //Подключаем библиотеку
     use Telegram\Bot\Api;
+    use RestClient\Client;
 
 $telegram = new Api('749857527:AAGMZgPom3lE7t_wHcxDC9YmTgRju_6Ll40'); //Устанавливаем токен, полученный у BotFather
 $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
