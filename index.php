c4f79abb252b1a6206b2ecb55fced1082ed25c81
steamme <nikita258963@gmail.com>
2019-02-05 16:05:32 +0200

    11111

--- index.php
+++ index.php
@@ -7,46 +7,6 @@
  */
 
 
-error_reporting(E_ALL);
-
-echo "<h2>Соединение TCP/IP</h2>\n";
-
-/* Получаем порт сервиса WWW. */
-$service_port = "9761";
-
-/* Получаем IP-адрес целевого хоста. */
-$address = "31.202.46.87";
-
-/* Создаём сокет TCP/IP. */
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if ($socket === false) {
-    echo "Не удалось выполнить socket_create(): причина: " . socket_strerror(socket_last_error()) . "\n";
-} else {
-    echo "OK.\n";
-}
-
-echo "Пытаемся соединиться с '$address' на порту '$service_port'...";
-$result = socket_connect($socket, $address, $service_port);
-if ($result === false) {
-    echo "Не удалось выполнить socket_connect().\nПричина: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
-} else {
-    echo "OK.\n";
-}
-
-$in = "023";
-$out = '';
-
-echo "Отправляем запрос на состояние выходов";
-socket_write($socket, $in, strlen($in));
-echo "OK.\n";
-
-echo "Читаем ответ:\n\n";
-while ($out = socket_read($socket, 2048)) {
-   echo $out;
-}
-
-echo "Закрываем сокет...";
-socket_close($socket);
-echo "OK.\n\n";
+echo "<p>Hello world from dorbot3</p>>";
 ?>
 
